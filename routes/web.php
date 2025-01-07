7bb901e0bf8f835cf85db4e331fa06fce7e48348
tunning_brain <tunning_brain@mail.ru>
2025-01-07 07:19:06 +0300

    local

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/jobs', function () {
+    return '<h1>This is my jobs</h1>';
+})->name('jobs');
+
+Route::match(['get', 'post'], '/submit', function () {
+    return 'was submitted';
+});
+
+Route::get('/test', function () {
+    $url = route('jobs');
+    return "<a href='$url'>Click here</a>";
+});
+
+
+Route::get('/api/users/1', fn() => [
+    'login' => 'alexneo68',
+    'email' => 'info@makewww.ru'
+]);
