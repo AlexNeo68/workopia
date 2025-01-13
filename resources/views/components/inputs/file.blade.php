9d7899d23a34cbdf455a427f2b17bde0ca1fb764
tunning_brain <tunning_brain@mail.ru>
2025-01-13 07:56:25 +0300

    before 008

---
+++ resources/views/components/inputs/file.blade.php
@@ -0,0 +1,11 @@
+@props(['id', 'name', 'label' => null])
+<div class="mb-4">
+    @if ($label)
+        <label class="block text-gray-700" for="{{ $id }}">{{ $label }}</label>
+    @endif
+    <input id="{{ $id }}" type="file" name="{{ $name }}"
+        class="w-full px-4 py-2 border rounded focus:outline-none @error($name) border-red-500 @enderror" />
+    @error($name)
+        <div class="text-red-500 text-xs">{{ $message }}</div>
+    @enderror
+</div>
---
+++ resources/views/components/inputs/text.blade.php
@@ -0,0 +1,15 @@
+@props(['id', 'type' => 'text', 'name', 'label' => null, 'value' => '', 'placeholder' => ''])
+<div class="mb-4">
+    @if ($label)
+        <label class="block text-gray-700" for="{{ $name }}">
+            {{ $label }}
+        </label>
+    @endif
+
+    <input id="{{ $name }}" type="{{ $type }}" name="{{ $name }}"
+        class="w-full px-4 py-2 border rounded focus:outline-none @error($name) border-red-500 @enderror"
+        placeholder="Software Engineer" value="{{ old($name, $value) }}" />
+    @error($name)
+        <div class="text-red-500 text-xs">{{ $message }}</div>
+    @enderror
+</div>
